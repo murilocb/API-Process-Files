f17fb3d0c76ffa606326bc5b00274e8a91893581
EsKeR <esker@localhost.localdomain>
2023-07-15 17:48:31 -0300

    feature: active 4 and 5, all boletos and options querys

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const fs = require("fs").promises;
 const path = require("path");
 const { processCSV } = require("../service/csvService");
+const { filtrarBoletos, gerarRelatorioPDF, allBoletos } = require('../service/getBoletosService')
 const pdfService = require("../service/pdfService");
 
 const router = express.Router();
@@ -25,6 +26,32 @@ router.post("/boleto", upload.single("file"), async (req, res) => {
     res.status(500).json({ error: "Error processing CSV file." });
   }
 });
+
+router.get(
+  "/boletos/:nome?/:valor_inicial?/:valor_final?/:id_lote?/:relatorio?",
+  async (req, res) => {
+    const { relatorio, nome, valor_inicial, valor_final, id_lote } = req.query;
+
+    if (relatorio) {
+      const relatorioPDF = await gerarRelatorioPDF();
+      const fileName = 'relatorio_boletos.pdf';
+      res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.send(relatorioPDF);
+    } else if (nome || valor_inicial || valor_final || id_lote) {
+      const boletosFiltrados = await filtrarBoletos(
+        nome,
+        valor_inicial,
+        valor_final,
+        id_lote
+      );
+      res.json(boletosFiltrados);
+    } else {
+      const boletosFiltrados = await allBoletos();
+      res.json(boletosFiltrados);
+    }
+  }
+);
 
 router.post("/pdf", upload.single("file"), async (req, res) => {
   try {
---
+++ src/service/getBoletosService.js
@@ -0,0 +1,104 @@
+const { Boletos } = require("../models");
+const PDFDocument = require("pdfkit");
+const { Op } = require("sequelize");
+
+async function filtrarBoletos(nome, valor_inicial, valor_final, id_lote) {
+  let where = {};
+
+  if (nome) {
+    where.nome_sacado = { [Op.like]: `%${nome}%` };
+  }
+  if (valor_inicial) {
+    where.valor = { [Op.gte]: parseFloat(valor_inicial) };
+  }
+  if (valor_final) {
+    where.valor = {
+      ...where.valor,
+      [Op.lte]: parseFloat(valor_final),
+    };
+  }
+  if (id_lote) {
+    where.id_lote = parseInt(id_lote);
+  }
+
+  const boletosFiltrados = await Boletos.findAll({ where });
+  return boletosFiltrados;
+}
+
+async function gerarRelatorioPDF() {
+  return new Promise((resolve, reject) => {
+    const doc = new PDFDocument();
+
+    doc.text("Relatório de Boletos", { align: "center", fontSize: 16 });
+    doc.moveDown();
+
+    Boletos.findAll()
+      .then((boletos) => {
+        const table = {
+          headers: ["ID", "Nome Sacado", "ID Lote", "Valor", "Linha Digitável"],
+          rows: boletos.map((boleto) => [
+            boleto.id,
+            boleto.nome_sacado,
+            boleto.id_lote,
+            boleto.valor.toFixed(2),
+            boleto.linha_digitavel,
+          ]),
+        };
+
+        const columnWidths = [60, 150, 60, 80, 150];
+
+        generateTable(doc, table, columnWidths);
+
+        const chunks = [];
+        doc.on("data", (chunk) => chunks.push(chunk));
+        doc.on("end", () => {
+          const pdfBytes = Buffer.concat(chunks);
+          resolve(pdfBytes);
+        });
+
+        doc.end();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function generateTable(doc, table, columnWidths) {
+  const { headers, rows } = table;
+
+  doc.font("Helvetica-Bold").fontSize(12);
+
+  const headerRow = headers.join(" | ");
+  doc.text(headerRow, {
+    width: columnWidths.reduce((a, b) => a + b),
+    align: "center",
+  });
+
+  const horizontalLine = "-".repeat(headerRow.length);
+  doc.text(horizontalLine, {
+    width: columnWidths.reduce((a, b) => a + b),
+    align: "center",
+  });
+
+  doc.font("Helvetica").fontSize(10);
+
+  for (const row of rows) {
+    const formattedRow = row.map((item, index) => {
+      const width = columnWidths[index] - 5;
+      return item.toString().padEnd(width);
+    });
+    doc.text(formattedRow.join(" | "), {
+      width: columnWidths.reduce((a, b) => a + b),
+      align: "center",
+    });
+  }
+}
+
+async function allBoletos() {
+  const boleto = await Boletos.findAll();
+
+  return boleto;
+}
+
+module.exports = { filtrarBoletos, gerarRelatorioPDF, allBoletos };
